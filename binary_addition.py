0edffed7f106d34c0f811376dfc4e85cd8db1874
Tina Fountain <56779579+tfo-ops@users.noreply.github.com>
2024-10-22 09:55:00 +0100

    Update binary_addition.py
    simplify code

--- binary_addition.py
+++ binary_addition.py
@@ -1,6 +1,6 @@
 from machine import Pin
 
-led_pins = [Pin(6, Pin.OUT), Pin(7, Pin.OUT), Pin(8, Pin.OUT), Pin(9, Pin.OUT)]
+led_pins = [Pin(2, Pin.OUT), Pin(3, Pin.OUT), Pin(4, Pin.OUT), Pin(5, Pin.OUT),Pin(6, Pin.OUT)]
 
 
 # Function for binary addition
@@ -10,7 +10,6 @@ def binary_addition(number_1, number_2):
 
     for i in range(3, -1, -1):
         bit_total = int(number_1[i]) + int(number_2[i]) + carry
-
         if bit_total == 0 or bit_total == 1:
             total = str(bit_total) + total
             carry = 0
@@ -19,21 +18,21 @@ def binary_addition(number_1, number_2):
             total = "0" + total
             carry = 1
             led_pins[i].value(0)
+            
         elif bit_total == 3:
             total = "1" + total
             carry = 1
             led_pins[i].value(1)
-
-    return total
+            
+    led_pins[4].value(carry)
+    print (carry,total)
 
 while True:
 
-	number_1 = input("Enter a 4-bit binary number: ")
-	number_2 = input("Enter a 2nd 4-bit binary number: ")
-
-
-	result = binary_addition(number_1, number_2)
-	print(result)
+    number_1 = input("Enter a 4-bit binary number: ")
+    number_2 = input("Enter a 2nd 4-bit binary number: ")
+    binary_addition(number_1, number_2)
+    
 
 
 
